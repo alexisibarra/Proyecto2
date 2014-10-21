21aa618d409086d5c2309c4b7b0119fcbb6dc565
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2014-10-21 12:50:40 -0430

    Añade la cabecera para el manejo de arreglos

---
+++ arrayHandling.h
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arrayHandling.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  21/10/14 12:08:27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+  void  printArray(int* array, int arrayLength)
+{
+  int i;
+
+  for(i=0; i < arrayLength; i++)
+    printf("%d\n",  array[i]);
+}
+
+  void
+genRandNumbersArray ( int numEnteros, int* numbersArray )
+{
+    int i, value;
+    srand(time(NULL));
+    for (i = 0; i < numEnteros; i++)
+    {
+        value = rand() % 10000 + 1;
+        numbersArray[i] = value;
+    }
+}		/* -----  end of function genRandNumbersArray  ----- */
